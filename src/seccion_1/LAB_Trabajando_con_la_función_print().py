819672bc3678a81aee0555368697c47cbd717414
Aly Santiago Cano <santiago19931916@gmail.com>
2026-04-22 20:18:20 -0500

    feat(seccion1): codigo de laboratorio print sin comentarios

---
+++ src/seccion_1/LAB_Trabajando_con_la_función_print().py
@@ -0,0 +1,11 @@
+# Autor: Aly Santiago Cano
+
+# 1. Imprimir ¡Hola, Mundo! con comillas dobles
+print("¡Hola, Mundo!")
+
+# 2. Imprimir mi nombre
+print("Aly Santiago Cano")
+
+# Experimentos con comillas simples y múltiples líneas
+print('Prueba con comillas simples')
+print("Línea A"); print("Línea B") # Uso de punto y coma para misma línea
